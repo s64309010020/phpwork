a5ac255804fe79290f7436aaedc6dd18961acc10
s64309010020 <s64309010020@ptnicec.ac.th>
2022-10-20 12:43:58 +0700

    ส่ง datatype,function_string

---
+++ datatype.php
@@ -0,0 +1,28 @@
+<?php
+
+    //String
+    $grade = "A";
+    $subject = "HTML";
+    $name = 'Hamidah';
+    $age = "22";
+
+    //Integer Number
+    $timeout = 30;
+    $score = 99;
+    $loan = -19;
+    $money = +88;
+
+    //Floating point
+    $gpa = 3.33;
+    $inits = 0.25;
+    $temp = -2.22;
+    $lumen = +7600.99;
+    $maths = 2e10;
+    echo $maths;
+
+    //Boolean
+    $fname = true;
+    $male = false;
+    
+
+?>
