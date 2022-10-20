7516ea29ec500e9c1f6cb2b51b3e621ce6352b07
s64309010020 <s64309010020@ptnicec.ac.th>
2022-10-20 11:13:50 +0700

    ไฟล์ variable/test

---
+++ variable.php
@@ -0,0 +1,19 @@
+<?php
+
+    $fname = 2;
+    $a1111 = 4;
+    $_test = 3.33;
+    $Age = 100;
+    $age = 99;
+    $fullname = "Hamidah Masae";
+    $prefix = "Mr.";
+
+    echo $fname."<br>";
+    echo $a1111;
+    echo "<br>".$Age."<br>";
+    echo $prefix.$fullname."<br>";
+    echo $Age + $age."<br>";
+    echo $Age." + ".$age." = ".($Age + $age);
+    print "<br>".$_test;
+
+?>
